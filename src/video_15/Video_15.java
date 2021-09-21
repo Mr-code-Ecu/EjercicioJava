18f1c1871c51a0945d95cee272a97e06c574a525
joselo <joselomanzano@hotmail.com>
2021-09-21 10:41:07 -0500

    ejercicio_1

---
+++ src/video_15/Video_15.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video_15;
+
+/**
+ *
+ * @author josel
+ */
+public class Video_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+float guille,luis,juan,suma;
+guille = 40;
+luis = guille/2;
+juan = (luis+guille)/2;
+        System.out.println("guille "+guille);
+        System.out.println("luis "+luis);
+        System.out.println("juan "+juan);
+suma = guille+luis+juan;        
+        System.out.println("la suma de todos es "+suma);
+
+    }
+    
+}
